254dc674e7ca074365c7071b65ecd949c3f33bc3
arshy-gray <arshy.gray@gmail.com>
2023-06-10 20:33:23 +0900

    #76 chore: gulp-gh-pages 추가

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,7 +4,8 @@ const gulp = require("gulp"),
     scope: ["devDependencies"],
   }),
   isProduction = require("./gulp/config/gulp.env"),
-  gulpConfig = require("./gulp/config/gulp.config");
+  gulpConfig = require("./gulp/config/gulp.config"),
+  deploy = require("gulp-gh-pages");
 
 require("./gulp/cleanTask.js")(gulp, $, gulpConfig);
 require("./gulp/htmlTask.js")(gulp, $, gulpConfig);
@@ -16,3 +17,7 @@ require("./gulp/purifyTask.js")(gulp, $, gulpConfig);
 require("./gulp/buildTask.js")(gulp, $, gulpConfig);
 
 gulp.task("default", gulp.series("cssClean", "scssSprite", "scssTask"));
+
+gulp.task("deploy", function () {
+  return gulp.src("./dist/**/*").pipe(deploy());
+});
