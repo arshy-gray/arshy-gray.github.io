d836bf1927cc9b1e8d09d7ad552dbb7a91a5a150
arshy-gray <arshy.gray@gmail.com>
2023-07-01 12:13:59 +0900

    #172 chore: jsTask sourcemap 추가

--- gulp/jsTask.js
+++ gulp/jsTask.js
@@ -19,15 +19,19 @@ module.exports = (gulp, $, config) => {
   jsLibTask.description = "js 라이브러리 파일을 dist로 복사합니다";
 
   function jsPrdTask() {
-    return gulp
-      .src(config.jsPrd.src, { since: gulp.lastRun("jsPrdTask") })
-      // .pipe(concat("arshy.min.js"))
-      .pipe(uglify())
-      .pipe(mode.production($.replace("/src/", "../")))
-      .on("error", (err) => {
-        console.log(err);
-      })
-      .pipe(gulp.dest(config.jsPrd.dest));
+    return (
+      gulp
+        .src(config.jsPrd.src, { since: gulp.lastRun("jsPrdTask") })
+        // .pipe(concat("arshy.min.js"))
+        .pipe($.sourcemaps.init())
+        .pipe(uglify())
+        .pipe(mode.production($.replace("/src/", "../")))
+        .on("error", (err) => {
+          console.log(err);
+        })
+        .pipe($.sourcemaps.write("./"))
+        .pipe(gulp.dest(config.jsPrd.dest))
+    );
   }
   jsPrdTask.description = "js 자체 제작 파일을 압축 후 한 개의 파일로 합쳐 dist로 복사합니다";
 
