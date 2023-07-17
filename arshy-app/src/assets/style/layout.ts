6d069a0b948ecf2e31efaf87534a9365a53ce990
arshy-gray <arshy.gray@gmail.com>
2023-07-18 01:01:36 +0900

    #191 refactor: 기본 스타일 수정

--- arshy-app/src/assets/style/layout.ts
+++ arshy-app/src/assets/style/layout.ts
@@ -4,15 +4,15 @@ import { IStyle } from './style';
  * # break_points
  * ### 반응형 변경점
  * ---------------------
- * | key | value | description |
+ * | key | value |
  * | :--- | :--- | :--- |
- * | pc_wide(string) | 1619px |  |
- * | pc_dft(string) | 1399px |  |
- * | tablet(string) | 1023px |  |
- * | mobile_high(string) | 767px |  |
- * | mobile_mid(string) | 599px |  |
- * | mobile_low(string) | 399px |  |
- * | minimum(string) | 320px |  |
+ * | pc_wide(string) | 1619px |
+ * | pc_dft(string) | 1399px |
+ * | tablet(string) | 1023px |
+ * | mobile_high(string) | 767px |
+ * | mobile_mid(string) | 599px |
+ * | mobile_low(string) | 399px |
+ * | minimum(string) | 320px |
  */
 export const break_points: IStyle = {
   pc_wide: '1619px',
